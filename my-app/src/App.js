58922b840bec8d4089a80861a5eb07091ef5dfb5
Moses Okode <okodekings76@gmail.com>
2024-11-20 17:45:04 +0300

    inital commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import Main from './Components/Main';
+import Navbar from './Components/Navbar';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      
+    </div>
+  );
+}
+
+export default App;
